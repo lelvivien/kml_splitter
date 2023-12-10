2d7fc4a00272ce4c00b2d885515db5651b59088f
soufiane <souphianezahiri@gmail.com>
2023-12-10 12:52:58 +0100

    first commit

---
+++ js/import_kml.js
@@ -0,0 +1,29 @@
+// import { kml } from "./togeojson.js";
+
+
+// var xml=""
+
+// document.getElementById('myfile')
+// .addEventListener('change', function () {
+
+
+//         let fr = new FileReader();
+//     fr.onload = function () {
+//         xml = fr.result
+//         document.getElementById('txt')
+//             .textContent = xml;
+           
+//     }
+//     fr.readAsBinaryString(this.files[0])
+//      var features =    kml(new DOMParser().parseFromString(xml, "text/xml"))
+     
+//      console.log(features)
+     
+  
+  
+   
+   
+ 
+   
+    
+// })
